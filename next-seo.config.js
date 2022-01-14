57fc11995d388f241280913629af4dbd82271240
randhypi <randhypradanairsan@gmail.com>
2022-01-14 10:11:01 +0700

    first commit

---
+++ next-seo.config.js
@@ -0,0 +1,36 @@
+const defaultSEOConfig = {
+  title: "template-nextjs-tailwind-starter",
+  titleTemplate: "%s | template-nextjs-tailwind-starter",
+  defaultTitle: "template-nextjs-tailwind-starter",
+  description:
+    "Yehezkiel Gunawan's personalized Next.js + chakra-ui + TypeScript starter template",
+  canonical: "https://template-nextjs-tailwind-starter.randhypi.com",
+  openGraph: {
+    url: "https://template-nextjs-tailwind-starter.randhypi.com",
+    title: "template-nextjs-tailwind-starter",
+    description: "Next.js + Tailwindcss + TypeScript template",
+    type: "website",
+    images: [
+      {
+        url: "https://yehez-og-image.yehezgun.com/**template-nextjs-tailwind-starter**.yehezgun.com.png?theme=dark&md=1&fontSize=50px&images=https%3A%2F%2Fres.cloudinary.com%2Fyehez%2Fimage%2Fupload%2Fv1631970666%2Fyehez_avatar_vkv7pc.png&widths=200&heights=200",
+        alt: "template-nextjs-tailwind-starter.yehezgun.com og-image",
+        width: 800,
+        height: 600,
+      },
+    ],
+    site_name: "template-nextjs-tailwind-starter",
+  },
+  twitter: {
+    handle: "@handle",
+    site: "@site",
+    cardType: "summary_large_image",
+  },
+  additionalLinkTags: [
+    {
+      rel: "icon",
+      href: "https://res.cloudinary.com/yehez/image/upload/v1630902976/Assassination_Classroom_-_Koro-sensei_smiling_head_fwpndi.svg",
+    },
+  ],
+};
+
+export default defaultSEOConfig;
